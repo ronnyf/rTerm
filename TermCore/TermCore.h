2d2270a8f44f0a5f0ccac873894a488a86b3a3f7
Ronny F <ronnyf@icloud.com>
2024-06-20 09:34:23 -0700

    added license info to source and header files

--- TermCore/TermCore.h
+++ TermCore/TermCore.h
@@ -2,7 +2,22 @@
 //  TermCore.h
 //  TermCore
 //
-//  Created by Ronny Falk on 6/19/24.
+//  Created by Ronny F on 6/19/24.
+//
+//  This file is part of rTerm.
+//
+//  Terminal App is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  Terminal App is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with Terminal App. If not, see <https://www.gnu.org/licenses/>.
 //
 
 #import <Foundation/Foundation.h>
@@ -13,6 +28,5 @@ FOUNDATION_EXPORT double TermCoreVersionNumber;
 //! Project version string for TermCore.
 FOUNDATION_EXPORT const unsigned char TermCoreVersionString[];
 
-// In this header, you should import all the public headers of your framework using statements like #import <TermCore/PublicHeader.h>
-
-
+// In this header, you should import all the public headers of your framework
+// using statements like #import <TermCore/PublicHeader.h>
--- rTerm/ContentView.swift
+++ rTerm/ContentView.swift
@@ -2,8 +2,23 @@
 //  ContentView.swift
 //  rTerm
 //
-//  Created by Ronny Falk on 6/19/24.
+//  Created by Ronny F on 6/19/24.
 //
+//  This file is part of rTerm.
+// 
+//  Terminal App is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+// 
+//  Terminal App is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//  GNU General Public License for more details.
+// 
+//  You should have received a copy of the GNU General Public License
+//  along with Terminal App. If not, see <https://www.gnu.org/licenses/>.
+// 
 
 import SwiftUI
 
